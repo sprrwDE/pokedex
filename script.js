2aa829360685886a788686d2ea5b3eceb0b726e9
Kai Schulz <info@sparrowmedia.de>
2024-08-08 20:26:18 +0200

    testcards with names

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 function init() {
-    fetchTest("");
+    fetchPokemon("");
 };
 
 const testUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-async function fetchTest(path = "") {
+async function fetchPokemon(path = "") {
     let response = await fetch(testUrl + path);
     let responseToJson = await response.json();
-    renderTest(responseToJson);
+    renderNames(responseToJson);
 }
 
-function renderTest(response){
+function renderNames(response) {
     const results = response.results;
-    for (let i = 0; i < results.length; i++) {
-        console.log(results[i].name);
+    for (let indexResults = 0; indexResults < results.length; indexResults++) {
+        let content = document.getElementById('content');
+        let name = results[indexResults].name;
+        content.innerHTML += testTemplateBootstrap(name);
     }
+}
+
+// Loadingscreen onload body
+
+// Es soll eine bestimmte Anzahl an Pokemon Karten direkt gerendert werden. Am besten zwischen 20 und 40.
+
+/* Unten gibt es einen Button, um weitere 20-40 Pokemon zu laden. (Info: insgesamt gibt es über 1000 Pokemon)
+    Es erscheint ein Loadingscreen (Userfeedback).
+    Der Button kann während des Ladens nicht erneut angeklickt werden. 
+*/
+
+/**
+Sichtbar soll auf jeder kleinen Pokemon Karte sein:
+    Name
+    Typ/en
+    Bild des Pokemons
+    Hintergrundfarbe passend zum Typ
+    ID (optional)
+ */
+
+function testTemplateBootstrap(name) {
+    return `<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+            card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>`
 }
