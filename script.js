828db320a23e047f3841fd063df2eda6cad83d58
Kai Schulz <info@sparrowmedia.de>
2024-08-08 20:17:28 +0200

    pokemon namen ausloggen

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const testUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 async function fetchTest(path = "") {
     let response = await fetch(testUrl + path);
-    let responseToJson = response.json();
-    console.log(responseToJson)
+    let responseToJson = await response.json();
+    renderTest(responseToJson);
+}
+
+function renderTest(response){
+    const results = response.results;
+    for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+    }
 }
