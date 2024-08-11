a9a35ae349e2877fbb7dafb5e8cfa807ab9e47b3
Kai Schulz <info@sparrowmedia.de>
2024-08-11 12:54:09 +0200

    types & style small cards

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,9 +1,19 @@
 function cardTemplate(pokemon, p) {
-    return `<div class="card small ${pokemon.types[0]}" style="width: 18rem;" id="card${p}" onclick="openDialog(${p})">
+    return `
+    <div class="card small ${pokemon.types[0]}" style="width: 18rem;" id="card${p}" onclick="openDialog(${p})">
+    <div class="type-wrapper-small">
+    <div class="type-img ${pokemon.types[0]}img">
+        <img class="type-img" src="assets/icons/types/${pokemon.types[0]}.svg">
+    </div>
+    ${pokemon.types[1] ? `
+    <div class="type-img ${pokemon.types[1]}img">
+        <img class="type-img" src="assets/icons/types/${pokemon.types[1]}.svg">
+    </div>
+    ` : ''}
+    </div>
     <img src="${pokemon.img}" class="card-img-top" alt="${pokemon.name}">
     <div class="card-body">
         <h5 class="card-title">${pokemon.name}</h5>
-        <p class="card-text">Hello Ane LOL LOL LOL LOL LOL LOL</p>
     </div>
 </div>`;
 }
@@ -13,6 +23,16 @@ function bigCardTemplate(pokemon, j) {
     <div class="dialog ${pokemon.types[0]}">
     <img class="big" src="${pokemon.bigImg}" class="card-img-top" alt="...">
     <h3>${pokemon.name}</h3>
+    <div class="type-wrapper-big">
+    <div class="type-img ${pokemon.types[0]}img">
+        <img class="type-img" src="assets/icons/types/${pokemon.types[0]}.svg">
+    </div>
+    ${pokemon.types[1] ? `
+    <div class="type-img ${pokemon.types[1]}img">
+        <img class="type-img" src="assets/icons/types/${pokemon.types[1]}.svg">
+    </div>
+    ` : ''}
+    </div>
     <div class = "big-content-wrapper">
     <div class="button-wrapper bigcard">    
     <button class="btn btn-dark stretch" onclick="switch1(${j})">c1</button>
